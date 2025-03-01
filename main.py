fdf875aeddcbdf0ac896bcfbb75fbf411463468a
sfd2004 <logiv66@gmail.com>
2025-03-01 20:10:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
-app = Flask(__name__)
+app = Flask(__name__) #dsdsdssd
 app.secret_key = 'supersecretkey'
 
 users, recipes = {}, []
